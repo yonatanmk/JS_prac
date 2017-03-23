63a2e916f45a6d80e798b576ed7932db1d4d39d7
Yonatan MK <yonatanmk@gmail.com>
2017-03-23 16:46:46 -0400

    navbar-react created functional dropdown

--- navbar-react/src/components/Dropdown.js
+++ navbar-react/src/components/Dropdown.js
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Dropdown = (props) => {
-  // debugger;
-  let counter = 0;
-  let items = props.items.map((item)=>{
-    counter ++;
+class Dropdown extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: false
+    };
+
+    this.toggleState = this.toggleState.bind(this);
+  }
+
+  toggleState () {
+    this.setState({ display: !this.state.display} );
+  }
+
+  render () {
+    let items;
+    let counter = 0;
+    if (this.state.display) {
+      items = this.props.items.map((item)=>{
+        counter ++;
+        return (
+          <li key={counter}>
+            <a href={item.link}>{item.text}</a>
+          </li>
+        );
+      });
+    }
+
     return (
-      <li key={counter}>
-        <a href={item.link}>{item.text}</a>
-      </li>
+      <div>
+        <a href="#!" onClick={this.toggleState}>{this.props.label + ' ▾'}</a>
+        <ul className="nav-dropdown">
+          {items}
+        </ul>
+      </div>
     );
-  });
-
-  return (
-    <div>
-      <a href="#!">{props.label + ' ▾'}</a>
-      <ul className="nav-dropdown">
-        {items}
-      </ul>
-    </div>
-  );
-};
+  }
+}
 
 export default Dropdown;
